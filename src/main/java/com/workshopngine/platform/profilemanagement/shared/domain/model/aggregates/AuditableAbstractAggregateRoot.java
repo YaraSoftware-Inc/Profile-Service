0c0423860abcb5072d0eeb439f2c93131e75d20e
Eric <erich56000@gmail.com>
2025-05-24 17:08:58 -0500

    feat: add auditable model for aggregate and entity

---
+++ src/main/java/com/workshopngine/platform/profilemanagement/shared/domain/model/aggregates/AuditableAbstractAggregateRoot.java
@@ -0,0 +1,31 @@
+package com.workshopngine.platform.profilemanagement.shared.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.domain.AbstractAggregateRoot;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class AuditableAbstractAggregateRoot<T extends AbstractAggregateRoot<T>> extends AbstractAggregateRoot<T> {
+
+    @Id
+    @Setter
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private Date createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private Date updatedAt;
+
+}
